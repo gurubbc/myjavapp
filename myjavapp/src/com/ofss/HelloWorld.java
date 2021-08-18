e3f8443a5e543e9f88eecb994132f35845d89bac
Guru <gurumurthy.ramamurthy@gmail.com>
2021-08-18 16:13:13 +0530

    "added new method add"

--- myjavapp/src/com/ofss/HelloWorld.java
+++ myjavapp/src/com/ofss/HelloWorld.java
@@ -2,8 +2,14 @@ package com.ofss;
 
 public class HelloWorld {
 
+	public static int add(int a, int b)
+	{
+		return a+b;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello Github");
+		System.out.println(add(5,6));
 
 	}
 
